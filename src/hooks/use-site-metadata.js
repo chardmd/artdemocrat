45a7a5fc3b705b87d07a3acfd7a156e7a3ff2121
Richard Dimalanta <chardmd.au@gmail.com>
2020-07-06 17:19:18 +1000

    Create use-site-metadata.js

---
+++ src/hooks/use-site-metadata.js
@@ -0,0 +1,22 @@
+import { useStaticQuery, graphql } from "gatsby";
+
+const useSiteMetadata = () => {
+  const { site } = useStaticQuery(
+    graphql`
+      query SiteMetaData {
+        site {
+          siteMetadata {
+            title
+            siteUrl
+            description
+            author
+          }
+        }
+      }
+    `
+  );
+
+  return site.siteMetadata;
+};
+
+export default useSiteMetadata;
