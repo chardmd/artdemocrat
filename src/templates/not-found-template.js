7a7cdef959ef4a5d1ae8141d8acb1b9d60a8cabd
Richard Dimalanta <chardmd.au@gmail.com>
2020-07-11 09:23:27 +1000

    added 404 route

---
+++ src/templates/not-found-template.js
@@ -0,0 +1,13 @@
+// @flow strict
+import React from "react";
+import Layout from "../layouts"; // layout should be the last one, to prevent the 'conflicting order' warning during 'yarn run build'
+
+const NotFoundTemplate = ({ location }) => (
+  <Layout location={location}>
+    <div>
+      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    </div>
+  </Layout>
+);
+
+export default NotFoundTemplate;
